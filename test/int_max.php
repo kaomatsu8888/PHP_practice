62b9f99ad56b1e6ce2199a8e4f887dfb1696b0e8
LAPTOP-28DR15R7\frontier-Python <m.kaoru0830@gmail.com>
2024-05-09 10:44:10 +0900

    Initial commit

---
+++ test/int_max.php
@@ -0,0 +1,3 @@
+<?php
+    echo PHP_INT_MAX; # echoの意味は後述. PHP_INT_MAXはint型の最大値を表す定数
+?>
---
+++ test/int_plus.php
@@ -0,0 +1,6 @@
+<pre>
+<?php
+  var_dump(PHP_INT_MAX); # var_dumpの意味は出力する変数の型を表示する. PHP_INT_MAXはint型の最大値を表す定数
+  var_dump(PHP_INT_MAX + 1); # PHP_INT_MAXに1を足す
+?>
+</pre>
