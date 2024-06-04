6816ffc97c7aefb73d548fd60a6f45b8bc693f4b
LAPTOP-28DR15R7\frontier-Python <m.kaoru0830@gmail.com>
2024-06-04 12:05:08 +0900

    php

--- tennis/index.php
+++ tennis/index.php
@@ -1,9 +1,27 @@
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-  <title>テニスサークル交流サイト</title>
-</head>
-<body>
-  <h1>テニスサークル交流サイト</h1>
-</body>
+<?php $info = file_get_contents("info.txt"); ?>
+<!doctype html>
+<html lang="ja" >
+  <head>
+    <title>サークルサイト</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+
+    <?php include('navbar.php'); ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+      <div>
+        <!-- ここから「本文」-->
+
+        <h1>お知らせ</h1>
+        <p><?php echo $info; ?></p>
+
+        <!-- 本文ここまで -->
+      </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
