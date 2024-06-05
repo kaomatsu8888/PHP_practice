f6a3b8db5209f1562798680527c2d0953d02b324
LAPTOP-28DR15R7\frontier-Python <m.kaoru0830@gmail.com>
2024-06-05 14:45:18 +0900

    php

--- php/chapter7/cart-insert.php
+++ php/chapter7/cart-insert.php
@@ -2,21 +2,21 @@
 <?php require '../header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-$id=$_REQUEST['id'];
-if (!isset($_SESSION['product'])) {
-	$_SESSION['product']=[];
+$id=$_REQUEST['id']; /* カートに追加する商品のID */
+if (!isset($_SESSION['product'])) { /* カートに商品がない場合 */
+	$_SESSION['product']=[]; /* カートに商品を初期化 */
 }
-$count=0;
-if (isset($_SESSION['product'][$id])) {
-	$count=$_SESSION['product'][$id]['count'];
+$count=0; /* カート内の商品数 */
+if (isset($_SESSION['product'][$id])) { /* カートに商品がある場合 */
+	$count=$_SESSION['product'][$id]['count']; /* カート内の商品数を取得 */
 }
-$_SESSION['product'][$id]=[
-	'name'=>$_REQUEST['name'], 
-	'price'=>$_REQUEST['price'], 
-	'count'=>$count+$_REQUEST['count']
+$_SESSION['product'][$id]=[ /* カートに追加する商品のID、名前、価格、数量 */
+	'name'=>$_REQUEST['name'], /*  商品名 */
+	'price'=>$_REQUEST['price'], /* 価格 */
+	'count'=>$count+$_REQUEST['count'] /* 数量 */
 ];
-echo '<p>カートに商品を追加しました。</p>';
-echo '<hr>';
-require 'cart.php';
+echo '<p>カートに商品を追加しました。</p>'; /* カートに商品を追加した旨を表示 */
+echo '<hr>'; /* 水平線を表示 */
+require 'cart.php'; /* カートの中身を表示 */
 ?>
 <?php require '../footer.php'; ?>
--- php/chapter7/logout-output.php
+++ php/chapter7/logout-output.php
@@ -2,8 +2,8 @@
 <?php require '../header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-if (isset($_SESSION['customer'])) {
-	unset($_SESSION['customer']);
+if (isset($_SESSION['customer'])) { /* issetは変数がセットされているかどうかを調べるための関数。 */
+	unset($_SESSION['customer']); /* unsetは変数を削除するための関数。 */
 	echo 'ログアウトしました。';
 } else {
 	echo 'すでにログアウトしています。';
